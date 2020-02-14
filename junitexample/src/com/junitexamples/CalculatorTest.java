73198d36257e727a66e6257e49fbfbd8f7def5ce
hp <hp@DESKTOP-U78EJ0R>
2020-02-14 14:08:13 +0530

    change

--- junitexample/src/com/junitexamples/CalculatorTest.java
+++ junitexample/src/com/junitexamples/CalculatorTest.java
@@ -9,22 +9,22 @@ public class CalculatorTest {
 	@Test
 	public void testAdd() {
 		Calculator c=new Calculator();
-		assertEquals(5,c.add(2,3));
+		assertEquals(9,c.add(5,4));
 	}
 
 	@Test
 	public void testSub() {
 		Calculator c=new Calculator();
-		assertEquals(-1,c.sub(2, 3));
+		assertEquals(1,c.sub(5, 4));
 	}
 	@Test
 	public void testMul() {
 		Calculator c=new Calculator();
-		assertEquals(6,c.mul(2, 3));
+		assertEquals(20,c.mul(5,4));
 	}
 	@Test
 	public void testDiv() {
 		Calculator c=new Calculator();
-		assertEquals(0,c.div(2, 3));
+		assertEquals(0,c.div(5, 4));
 	}
 }
